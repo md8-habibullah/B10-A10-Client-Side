c57ca1dbe245529e4d115287dee7e313dce98567
md8-habibullah <>
2025-06-19 11:59:35 +0600

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import './App.css'
+import { Home } from './pages/home/Home'
 
-const App = () => {
+function App() {
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Home></Home>
+    </>
   )
 }
 
